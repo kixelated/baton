7a5e7bfc4e27ea524902efd08a9fc195549dfc03
Luke Curley <kixelated@gmail.com>
2025-01-28 16:39:14 -0300

    Use tokio::watch instead.

--- baton/src/recv.rs
+++ baton/src/recv.rs
@@ -1,45 +1,32 @@
-use std::sync::{Arc, Mutex};
-
-use crate::State;
+use tokio::sync::watch;
 
 /// The receiving half.
 /// Returns or waits for each update, potentially skipping them if too slow.
 #[derive(Clone)]
 pub struct Recv<T: Clone> {
-    state: Arc<Mutex<State<T>>>,
-    epoch: usize,
+    latest: T,
+    watch: watch::Receiver<T>,
 }
 
 impl<T: Clone> Recv<T> {
-    pub(crate) fn new(state: Arc<Mutex<State<T>>>) -> Self {
-        Self { state, epoch: 0 }
+    pub(crate) fn new(watch: watch::Receiver<T>) -> Self {
+        let latest = watch.borrow().clone();
+        Self { latest, watch }
     }
 
-    /// Wait for an unseen value, including the initial value.
+    /// Wait for an unseen value, not including the initial value.
     /// Returns [None] when the [Send] half is dropped with no new value.
     ///
     /// This only returns the latest value so some values may be skipped.
     /// If you want every value, use one of the many channel implementations.
-    pub async fn next(&mut self) -> Option<T> {
-        loop {
-            let notify = {
-                let mut state = self.state.lock().unwrap();
-                match state.next(self.epoch) {
-                    Ok((epoch, value)) => {
-                        self.epoch = epoch;
-                        return Some(value);
-                    }
-                    Err(Some(notify)) => notify,
-                    Err(None) => return None,
-                }
-            };
-
-            notify.notified().await;
-        }
+    pub async fn next(&mut self) -> Option<&T> {
+        self.watch.changed().await.ok()?;
+        self.latest = self.watch.borrow_and_update().clone();
+        Some(&self.latest)
     }
 
-    /// Return the latest value without waiting or marking it as seen.
-    pub fn get(&self) -> T {
-        self.state.lock().unwrap().get()
+    /// Return the initial value or the latest value returned by [Self::next].
+    pub fn get(&self) -> &T {
+        &self.latest
     }
 }
--- baton/src/send.rs
+++ baton/src/send.rs
@@ -1,59 +1,45 @@
-use std::sync::{Arc, Mutex};
+use tokio::sync::watch;
 
-use crate::{Recv, State};
+use crate::Recv;
 
 /// The sending half.
 /// Sends new values to the receiving half, potentially dropping them if too fast.
 #[derive(Clone)]
 pub struct Send<T: Clone> {
-    state: Arc<Mutex<State<T>>>,
-    // Close when all send handles are dropped.
-    _drop: Arc<SendDrop<T>>,
+    watch: watch::Sender<T>,
 }
 
 impl<T: Clone> Send<T> {
-    pub(crate) fn new(state: Arc<Mutex<State<T>>>) -> Self {
-        Self {
-            _drop: Arc::new(SendDrop::new(state.clone())),
-            state,
-        }
+    pub(crate) fn new(watch: watch::Sender<T>) -> Self {
+        Self { watch }
     }
 
     /// Set a new value.
     pub fn set(&mut self, value: T) {
-        self.state.lock().unwrap().set(value);
+        self.watch.send_replace(value);
     }
 
     /// Return the latest value.
     pub fn get(&self) -> T {
-        self.state.lock().unwrap().get()
+        self.watch.borrow().clone()
     }
 
     /// Create a new receiving half.
     pub fn recv(&self) -> Recv<T> {
-        Recv::new(self.state.clone())
+        Recv::new(self.watch.subscribe())
     }
 }
 
 impl<T: Clone + PartialEq> Send<T> {
     /// Set a new value if it is different from the previous one.
-    pub fn set_if(&mut self, value: T) {
-        self.state.lock().unwrap().set_if(value);
-    }
-}
-
-struct SendDrop<T: Clone> {
-    state: Arc<Mutex<State<T>>>,
-}
-
-impl<T: Clone> SendDrop<T> {
-    fn new(state: Arc<Mutex<State<T>>>) -> Self {
-        Self { state }
-    }
-}
-
-impl<T: Clone> Drop for SendDrop<T> {
-    fn drop(&mut self) {
-        self.state.lock().unwrap().close();
+    pub fn update(&mut self, value: T) {
+        self.watch.send_if_modified(|watch| {
+            if watch != &value {
+                *watch = value;
+                true
+            } else {
+                false
+            }
+        });
     }
 }
